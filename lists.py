b128bbd23ccb50da0d61b442e00b29779650fdfb
TPHsMP <o.g.kcity@gmail.com>
2022-08-23 09:17:30 +0600

    рассортировал изображения упражнений по папкам

--- lists.py
+++ lists.py
@@ -1,45 +1,46 @@
-biceps = {'barbellcurls.jpg': 'Подъем штанги на бицепс',
-          'zcurls.jpg': 'Подъем штанги на бицепс с Z-грифом',
-          'dumpbellscurls.jpg': 'Сгибания на бицепс с гантелями',
-          'hummercurls.jpg': 'Молот с гантелями',
-          'isolatedcurls.jpg': 'Изолированные сгибания',
-          'reversepullups.jpg': 'Подтягивания обратным хватом'}
+biceps = {'images/biceps/barbellcurls.jpg': 'Подъем штанги на бицепс',
+          'images/biceps/zcurls.jpg': 'Подъем штанги на бицепс с Z-грифом',
+          'images/biceps/dumpbellscurls.jpg': 'Сгибания на бицепс с гантелями',
+          'images/biceps/hummercurls.jpg': 'Молот с гантелями',
+          'images/biceps/isolatedcurls.jpg': 'Изолированные сгибания',
+          'images/biceps/reversepullups.jpg': 'Подтягивания обратным хватом'}
 
-triceps = {'frenchpress.jpg': 'Французский жим с штангой',
-           'dumpbell_frenchpress.jpg': 'Французский жим с гантелями'}
+triceps = {'images/triceps/frenchpress.jpg': 'Французский жим с штангой',
+           'images/triceps/dumpbell_frenchpress.jpg': 'Французский жим с гантелями'}
 
-wrists = {'dumpbellfingerpull.jpg': 'Тяга гантели пальцами',
+wrists = {'images/wrists/dumpbellfingerpull.jpg': 'Тяга гантели пальцами',
           'burpbellpull.jpg': 'Подъем грифа обратным хватом'}
 
-chest = {'benchpress.jpg': 'Жим лежа',
-         'benchpress_2.jpg': 'Жим лежа с положительным наклоном',
-         'benchpress_3.jpg': 'Жим лежа с отрицательным наклоном',
-         'narrowpress.jpg': 'Жим гантелей от груди узким хватом',
-         'dumpbellwiring.jpg': 'Разведение гантелей',
-         'crosswiring.jpg': 'Бабочка'}
+chest = {'images/chest/benchpress.jpg': 'Жим лежа',
+         'images/chest/benchpress_2.jpg': 'Жим лежа с положительным наклоном',
+         'images/chest/benchpress_3.jpg': 'Жим лежа с отрицательным наклоном',
+         'images/chest/narrowpress.jpg': 'Жим гантелей от груди узким хватом',
+         'images/chest/dumpbellwiring.jpg': 'Разведение гантелей',
+         'images/chest/crosswiring.jpg': 'Бабочка'}
 
-back = {'rodpull.jpg': 'Тяга штанги в наклоне',
-        't-pull.jpg': 'Тяга Т-грифа',
-        'dumpbellpull.jpg': 'Тяга гантели в наклоне',
-        'chestpull.jpg': 'Тяга верхнего блока к груди',
-        'scars.jpg': 'Шраги',
-        'frontpull.jpg': 'Фронтальная тяга',
-        'hyperestension.jpg': 'Гиперэкстензия',
-        'deadlift.jpg': 'Становая тяга'}
+back = {'images/back/rodpull.jpg': 'Тяга штанги в наклоне',
+        'images/back/t-pull.jpg': 'Тяга Т-грифа',
+        'images/back/dumpbellpull.jpg': 'Тяга гантели в наклоне',
+        'images/back/chestpull.jpg': 'Тяга верхнего блока к груди',
+        'images/back/scars.jpg': 'Шраги',
+        'images/back/frontpull.jpg': 'Фронтальная тяга',
+        'images/back/hyperestension.jpg': 'Гиперэкстензия',
+        'images/back/deadlift.jpg': 'Становая тяга',
+        'images/back/pullover.jpg': 'Пуловер'}
 
-shoulders = {'sittingdumpbellpress.jpg': 'Жим гантелей сидя',
-             'sittingdumpbellwiring.jpg': 'Разведение гантелей',
-             'barbellchestpull.jpg': 'Тяга штанги к груди',
-             'overheadpress.jpg': 'Жим штанги за голову',
-             'overheadpull.jpg': 'Тяга верхнего блока за голову',
-             'frontallift.jpg': 'Фронтальные подъемы (возможно выполнение с гантелями или с штангой'}
+shoulders = {'images/shoulders/sittingdumpbellpress.jpg': 'Жим гантелей сидя',
+             'images/shoulders/sittingdumpbellwiring.jpg': 'Разведение гантелей',
+             'images/shoulders/barbellchestpull.jpg': 'Тяга штанги к груди',
+             'images/shoulders/overheadpress.jpg': 'Жим штанги за голову',
+             'images/shoulders/overheadpull.jpg': 'Тяга верхнего блока за голову',
+             'images/shoulders/frontallift.jpg': 'Фронтальные подъемы (возможно выполнение с гантелями или с штангой'}
 
-legs = {'backsquats.jpg':'Приседения со штангой',
-        'lunges.jpg':'Выпады',
-        'legspress.jpg':'Жим ногами лежа',
-        'legsextensions.jpg':'Разгибания ног в тренажере',
-        'gakksquats.jpg':'Гакк приседания',
-        'chestsquats.jpg':'Приседания с штангой на груди'}
+legs = {'images/legs/backsquats.jpg':'Приседения со штангой',
+        'images/legs/lunges.jpg':'Выпады',
+        'images/legs/legspress.jpg':'Жим ногами лежа',
+        'images/legs/legsextensions.jpg':'Разгибания ног в тренажере',
+        'images/legs/gakksquats.jpg':'Гакк приседания',
+        'images/legs/chestsquats.jpg':'Приседания с штангой на груди'}
 
 press = {}
 
